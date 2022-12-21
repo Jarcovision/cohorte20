5bb936443e7f178755c2712e9c4106805ce98e17
José <jarcovision@cafetera.local>
2022-12-21 17:55:24 -0600

    JWT y React

---
+++ javaWeb/javaWeb-01/ejemplo-5/src/test/java/com/jlsg/myappjwt/Ejemplo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlsg.myappjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
